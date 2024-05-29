86c2a622611741b99b4b036e31ee74a4220f2489
TimurBerdyyev <gorilazanimals@gmail.com>
2024-05-29 16:33:04 +0600

    добавил в админ панель класс OwnerInline который подключен к FlatAdmin

--- property/admin.py
+++ property/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from .models import Flat, Сomplaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ['owner']
+
+
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'phone_nuber', 'pure_phone_number']
     search_fields = ['full_name', 'phone_nuber']
     raw_id_fields = ['flats']
 
 class FlatAdmin(admin.ModelAdmin):
+    inlines = (OwnerInline,)
     search_fields = ['town', 'address', 'owner', 'owner_pure_phone']
     readonly_fields = ['created_at']
     list_display = ['address', 'price','new_building', 'construction_year', 'town']
